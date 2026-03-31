5e97c2747de360d3438f6224e0de364200b7416b
C0ng_yun <s25069@gsm.hs.kr>
2026-03-31 13:24:05 +0900

    add : SignUpService

--- src/main/kotlin/congyun/toy/jobbo/domain/user/service/SignUpService.kt
+++
@@ -1,7 +0,0 @@
-package congyun.toy.jobbo.domain.user.service
-
-import congyun.toy.jobbo.domain.user.presentation.data.request.SignUpRequest
-
-interface SignUpService {
-    fun execute(request: SignUpRequest)
-}
