6670c3eadbaa7f18319dba3e7d112736fb992dee
C0ng_yun <s25069@gsm.hs.kr>
2026-03-31 13:23:18 +0900

    add : JwtProperties

---
+++ src/main/kotlin/congyun/toy/jobbo/global/jwt/JwtProperties.kt
@@ -0,0 +1,10 @@
+package congyun.toy.jobbo.global.jwt
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "jwt")
+data class JwtProperties(
+    val secret: String,
+    val accessTokenExpiration: Long,
+    val refreshTokenExpiration: Long,
+)
