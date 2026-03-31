3739af65ec9cfb0c964ebbb1aed50b52556599e8
C0ng_yun <s25069@gsm.hs.kr>
2026-03-31 13:24:21 +0900

    add : LoginRequest

--- src/main/kotlin/congyun/toy/jobbo/domain/auth/presentation/data/request/LoginRequest.kt
+++ src/main/kotlin/congyun/toy/jobbo/domain/auth/presentation/data/request/LoginRequest.kt
@@ -1,4 +1,4 @@
-package congyun.toy.jobbo.domain.user.presentation.data.request
+package congyun.toy.jobbo.domain.auth.presentation.data.request
 
 import jakarta.validation.constraints.Email
 import jakarta.validation.constraints.NotBlank
