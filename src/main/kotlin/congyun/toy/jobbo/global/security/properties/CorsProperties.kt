912c7e949b517b7e247f7b00be7c28fad286b731
C0ng_yun <s25069@gsm.hs.kr>
2026-03-31 13:23:18 +0900

    add : CorsProperties

---
+++ src/main/kotlin/congyun/toy/jobbo/global/security/properties/CorsProperties.kt
@@ -0,0 +1,9 @@
+package congyun.toy.jobbo.global.security.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+
+@ConfigurationProperties(prefix = "cors")
+class CorsProperties(
+    private val allowedOrigins : List<String>
+) {
+}
