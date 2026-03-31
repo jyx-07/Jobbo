fe696f07423dd56e6136b3f81aef4c02767cf688
C0ng_yun <s25069@gsm.hs.kr>
2026-03-31 13:23:19 +0900

    add : SecurityConfig

---
+++ src/main/kotlin/congyun/toy/jobbo/global/security/config/SecurityConfig.kt
@@ -0,0 +1,57 @@
+package congyun.toy.jobbo.global.security.config
+
+import co.elastic.clients.elasticsearch.watcher.EmailAttachmentBuilders.http
+import congyun.toy.jobbo.global.security.filter.JwtFilter
+import congyun.toy.jobbo.global.security.handler.JwtAccessDeniedHandler
+import congyun.toy.jobbo.global.security.handler.JwtAuthenticationEntryPoint
+import congyun.toy.jobbo.global.security.properties.CorsProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.security.web.server.SecurityWebFilterChain
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+class SecurityConfig(
+    private val jwtFilter: JwtFilter,
+    private val jwtAccessDeniedHandler: JwtAccessDeniedHandler,
+    private val jwtAuthenticationEntryPoint: JwtAuthenticationEntryPoint,
+    private val corsProperties: CorsProperties,
+
+
+) {
+    companion object{
+        private val PUBLIC_URL = arrayOf(
+            "/auth/**",
+            "/health/**",
+            "/error",
+        )
+    }
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .csrf { it.disable() }
+            .formLogin { it.disable() }
+            .httpBasic { it.disable() }
+            .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .exceptionHandling {
+                it.authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                it.accessDeniedHandler(jwtAccessDeniedHandler)
+            }
+            .authorizeHttpRequests {
+                it.requestMatchers(*PUBLIC_URL).permitAll()
+                it.anyRequest().authenticated()
+            }
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter::class.java)
+            .build()
+
+    }
+}
